e571a487317b62a9f3412dd4e4534d8524af1538
berkayersoy <berkay.ersoy.007@gmail.com>
2021-07-01 12:53:12 +0300

    Admin panel video add featured added

---
+++ src/app/components/admin-panel-video-add-video-modal/admin-panel-video-add-video-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-panel-video-add-video-modal',
+  templateUrl: './admin-panel-video-add-video-modal.component.html',
+  styleUrls: ['./admin-panel-video-add-video-modal.component.css']
+})
+export class AdminPanelVideoAddVideoModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
